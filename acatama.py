8d9244aec97f1f2c7ae02f8a0605eb1b367f7d37
Xavier Corredor Llano <11801453+XavierCLL@users.noreply.github.com>
2017-05-16 22:56:10 -0500

    Fixed and adjusted the menu icon

--- acatama.py
+++ acatama.py
@@ -49,10 +49,7 @@ class AcATaMa:
 
         # initialize locale
         locale = QSettings().value('locale/userLocale')[0:2]
-        locale_path = os.path.join(
-            self.plugin_dir,
-            'i18n',
-            'AcATaMa_{}.qm'.format(locale))
+        locale_path = os.path.join(self.plugin_dir, 'i18n', 'AcATaMa_{}.qm'.format(locale))
 
         if os.path.exists(locale_path):
             self.translator = QTranslator()
@@ -61,18 +58,13 @@ class AcATaMa:
             if qVersion() > '4.3.3':
                 QCoreApplication.installTranslator(self.translator)
 
-        # Declare instance attributes
-        self.actions = []
-        self.menu = self.tr(u'&AcATaMa')
-        # TODO: We are going to let the user set this up in a future iteration
-        self.toolbar = self.iface.addToolBar(u'AcATaMa')
-        self.toolbar.setObjectName(u'AcATaMa')
-
-        #print "** INITIALIZING AcATaMa"
+        print "** INITIALIZING AcATaMa"
 
         self.pluginIsActive = False
         self.dockwidget = None
 
+        # Obtaining the map canvas
+        self.canvas = iface.mapCanvas()
 
     # noinspection PyMethodMayBeStatic
     def tr(self, message):
@@ -89,97 +81,26 @@ class AcATaMa:
         # noinspection PyTypeChecker,PyArgumentList,PyCallByClass
         return QCoreApplication.translate('AcATaMa', message)
 
-
-    def add_action(
-        self,
-        icon_path,
-        text,
-        callback,
-        enabled_flag=True,
-        add_to_menu=True,
-        add_to_toolbar=True,
-        status_tip=None,
-        whats_this=None,
-        parent=None):
-        """Add a toolbar icon to the toolbar.
-
-        :param icon_path: Path to the icon for this action. Can be a resource
-            path (e.g. ':/plugins/foo/bar.png') or a normal file system path.
-        :type icon_path: str
-
-        :param text: Text that should be shown in menu items for this action.
-        :type text: str
-
-        :param callback: Function to be called when the action is triggered.
-        :type callback: function
-
-        :param enabled_flag: A flag indicating if the action should be enabled
-            by default. Defaults to True.
-        :type enabled_flag: bool
-
-        :param add_to_menu: Flag indicating whether the action should also
-            be added to the menu. Defaults to True.
-        :type add_to_menu: bool
-
-        :param add_to_toolbar: Flag indicating whether the action should also
-            be added to the toolbar. Defaults to True.
-        :type add_to_toolbar: bool
-
-        :param status_tip: Optional text to show in a popup when mouse pointer
-            hovers over the action.
-        :type status_tip: str
-
-        :param parent: Parent widget for the new action. Defaults None.
-        :type parent: QWidget
-
-        :param whats_this: Optional text to show in the status bar when the
-            mouse pointer hovers over the action.
-
-        :returns: The action that was created. Note that the action is also
-            added to self.actions list.
-        :rtype: QAction
-        """
-
-        icon = QIcon(icon_path)
-        action = QAction(icon, text, parent)
-        action.triggered.connect(callback)
-        action.setEnabled(enabled_flag)
-
-        if status_tip is not None:
-            action.setStatusTip(status_tip)
-
-        if whats_this is not None:
-            action.setWhatsThis(whats_this)
-
-        if add_to_toolbar:
-            self.toolbar.addAction(action)
-
-        if add_to_menu:
-            self.iface.addPluginToMenu(
-                self.menu,
-                action)
-
-        self.actions.append(action)
-
-        return action
-
-
     def initGui(self):
-        """Create the menu entries and toolbar icons inside the QGIS GUI."""
-
+        # Create action that will start plugin configuration
         icon_path = ':/plugins/AcATaMa/icon.png'
-        self.add_action(
-            icon_path,
-            text=self.tr(u''),
-            callback=self.run,
-            parent=self.iface.mainWindow())
+        self.action = QAction(
+            QIcon(icon_path),
+            self.tr(u'&AcATaMa'), self.iface.mainWindow())
+        # connect the action to the run method
+        self.action.triggered.connect(self.run)
+
+        # Add toolbar button and menu item
+        self.iface.addToolBarIcon(self.action)
+        self.iface.addPluginToMenu(self.tr(u"&Accuracy Assessment of Thematic Maps"), self.action)
 
     #--------------------------------------------------------------------------
 
     def onClosePlugin(self):
         """Cleanup necessary items here when plugin dockwidget is closed"""
 
-        #print "** CLOSING AcATaMa"
+        print "** CLOSING AcATaMa"
+        self.clear_all()
 
         # disconnects
         self.dockwidget.closingPlugin.disconnect(self.onClosePlugin)
@@ -196,15 +117,12 @@ class AcATaMa:
     def unload(self):
         """Removes the plugin menu item and icon from QGIS GUI."""
 
-        #print "** UNLOAD AcATaMa"
+        print "** UNLOAD AcATaMa"
+        self.clear_all()
 
-        for action in self.actions:
-            self.iface.removePluginMenu(
-                self.tr(u'&AcATaMa'),
-                action)
-            self.iface.removeToolBarIcon(action)
-        # remove the toolbar
-        del self.toolbar
+        # Remove the plugin menu item and icon
+        self.iface.removePluginMenu(self.tr(u"&Accuracy Assessment of Thematic Maps"), self.action)
+        self.iface.removeToolBarIcon(self.action)
 
     #--------------------------------------------------------------------------
 
@@ -231,3 +149,5 @@ class AcATaMa:
             self.iface.addDockWidget(Qt.RightDockWidgetArea, self.dockwidget)
             self.dockwidget.show()
 
+    def clear_all(self):
+        pass
