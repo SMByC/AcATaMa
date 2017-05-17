a510434a830bc538d5b53cd410bc4753bd48e8b6
Xavier Corredor <Xavier Corredor@localhost>
2017-05-17 18:57:53 -0500

    Implemented "About" dialog

---
+++ gui/about_dialog.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+ AcATaMa
+                                 A QGIS plugin
+ AcATaMa is a Qgis plugin for Accuracy Assessment of Thematic Maps
+                              -------------------
+        copyright            : (C) 2017 by Xavier Corredor Llano, SMBYC
+        email                : xcorredorl@ideam.gov.co
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+"""
+
+import os
+from PyQt4 import QtGui, uic
+
+# plugin path
+plugin_folder = os.path.dirname(os.path.dirname(__file__))
+FORM_CLASS, _ = uic.loadUiType(os.path.join(
+    plugin_folder, 'ui', 'about.ui'))
+
+
+class AboutDialog(QtGui.QDialog, FORM_CLASS):
+    def __init__(self):
+        QtGui.QDialog.__init__(self)
+        self.setupUi(self)
+        about_file = os.path.join(plugin_folder, 'gui', 'about.html')
+        html_text = open(about_file).read()
+        self.about_html.setHtml(html_text)
