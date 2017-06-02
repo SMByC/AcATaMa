e440295edefd5d86664dd0df4ae9396bb3d23333
Xavier Corredor Llano <xcorredorl@ideam.gov.co>
2017-06-02 16:57:37 -0500

    Decrease the time of message bar

--- core/raster.py
+++ core/raster.py
@@ -37,7 +37,7 @@ def do_clipping_with_shape(target_file, shape, out_path):
         return out_file
     else:
         iface.messageBar().pushMessage("AcATaMa", "Error while clipping the raster file with shape.",
-                                       level=QgsMessageBar.WARNING, duration=20)
+                                       level=QgsMessageBar.WARNING, duration=10)
 
 
 def get_extent(img_path):
@@ -61,7 +61,7 @@ def get_color_table(raster_path, band_number=1):
     colorTable = gdalBand.GetColorTable()
     if colorTable is None:
         iface.messageBar().pushMessage("AcATaMa", "Error, the raster file selected has no color table",
-                                       level=QgsMessageBar.WARNING, duration=20)
+                                       level=QgsMessageBar.WARNING, duration=10)
         return
 
     count = colorTable.GetCount()
