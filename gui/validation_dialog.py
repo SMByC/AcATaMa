c857126da7bd85153f5026c5d79bf96e42ff6984
Xavier Corredor Llano <xcorredorl@ideam.gov.co>
2017-09-01 21:52:00 -0500

    Save view widgets instances and setting the respective label names

--- gui/validation_dialog.py
+++ gui/validation_dialog.py
@@ -30,9 +30,19 @@ FORM_CLASS, _ = uic.loadUiType(os.path.join(
 
 class ValidationDialog(QtGui.QDialog, FORM_CLASS):
     sampling_layer = None
+    view_widgets = []
 
     def __init__(self, sampling_layer):
         QtGui.QDialog.__init__(self)
         ValidationDialog.sampling_layer = sampling_layer
 
         self.setupUi(self)
+
+        # save the six view render widgets
+        ValidationDialog.view_widgets = \
+            [self.render_window_1, self.render_window_2, self.render_window_3,
+             self.render_window_4, self.render_window_5, self.render_window_6]
+
+        # set the label names for each view
+        for num_view, view_widget in zip(range(1, 7), ValidationDialog.view_widgets):
+            view_widget.view_label_name.setText("View {}:".format(num_view))
