89e10f0536dd3090c97f51322b7c725951613f1f
Xavier Corredor Llano <xcorredorl@ideam.gov.co>
2017-09-06 19:30:14 -0500

    Added the dialog for set or modify the classification buttons

---
+++ core/classification.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+ AcATaMa
+                                 A QGIS plugin
+ AcATaMa is a Qgis plugin for Accuracy Assessment of Thematic Maps
+                              -------------------
+        copyright            : (C) 2017 by Xavier Corredor Llano, SMBYC
+        email                : xcorredorl@ideam.gov.co
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+"""
+
+
+class Classification:
+    instances = {}
+
+    def __init__(self, sampling_layer):
+        self.sampling_layer = sampling_layer
+        self.btns_config = None
+
+        Classification.instances[sampling_layer] = self
+
+
