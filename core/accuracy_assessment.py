34273c9258a8876869c8e9e11ba486a8d064c228
Xavier Corredor Llano <xcorredorl@ideam.gov.co>
2017-11-03 23:02:41 -0500

    Select/set the sampling file status and UI for accuracy assessment tab

---
+++ core/accuracy_assessment.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+ AcATaMa
+                                 A QGIS plugin
+ AcATaMa is a Qgis plugin for Accuracy Assessment of Thematic Maps
+                              -------------------
+        copyright            : (C) 2017 by Xavier Corredor Llano, SMBYC
+        email                : xcorredorl@ideam.gov.co
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+"""
+
+
+
+class AccuracyAssessment:
+
+    def __init__(self):
+        pass
