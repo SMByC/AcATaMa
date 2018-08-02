a6d9fbc4dab2fcf1196dd98e2fe12fac1fcad1ab
Xavier Corredor Llano <xcorredorl@ideam.gov.co>
2018-08-02 17:44:15 -0500

    Added a warning block if the thematic has a geographic units warning to user about the area accuracy (grafted from f36ef23cf656b928ab06e45968ddd2b888e20254)

--- gui/accuracy_assessment_results.py
+++ gui/accuracy_assessment_results.py
@@ -24,6 +24,8 @@ import csv
 import os
 import copy
 
+from qgis.core import QGis
+
 from AcATaMa.utils.qgis_utils import get_file_path_of_layer
 
 
@@ -73,6 +75,12 @@ def get_html(accu_asse):
         os.path.basename((get_file_path_of_layer(accu_asse.classification.sampling_layer)).encode('utf-8')))
     html += "<p><strong>Classification status:</strong> {}/{} samples classified</p>".format(
         accu_asse.classification.total_classified, accu_asse.classification.num_points)
+
+    # warning block if the thematic has a geographic units
+    if accu_asse.base_area_unit == QGis.Degrees:
+        html += "<p style='color:black;background-color:#ffc53a;white-space:pre;padding:4px'><strong>Warning!</strong><br/>" \
+                "The thematic raster has a geographic coordinate system, therefore all area values are not accurate.<br/>" \
+                "For fix that use the UTM coordinate system.</p>"
 
     # warning block for samples outside the thematic raster area or inside the no data values
     if accu_asse.samples_outside_the_thematic:
