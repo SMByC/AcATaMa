3eb5056f320543c2d339e1e390d5e87c3fef3ca0
Xavier Corredor Llano <xcorredorl@ideam.gov.co>
2019-02-28 00:34:01 -0500

    Refactoring and improve the function for compute in parallel the total pixel count for each pixel value, used in area based proportion and accuracy assessment result. With this fix too weird bug while computing the pixel counts for some big thematic image

--- utils/others_utils.py
+++ utils/others_utils.py
@@ -18,6 +18,13 @@
  *                                                                         *
  ***************************************************************************/
 """
+import numpy as np
+import multiprocessing
+import xml.etree.ElementTree as ET
+from osgeo import gdal
+
+from AcATaMa.utils.qgis_utils import get_file_path_of_layer
+from AcATaMa.utils.system_utils import wait_process
 
 
 def mask(input_list, boolean_mask):
@@ -32,3 +39,72 @@ def mask(input_list, boolean_mask):
         ['A', 'C']
     """
     return [i for i, b in zip(input_list, boolean_mask) if b]
+
+# --------------------------------------------------------------------------
+
+
+def get_pixel_values(layer, band):
+    current_style = layer.styleManager().currentStyle()
+    layer_style = layer.styleManager().style(current_style)
+    xml_style_str = layer_style.xmlData()
+    xml_style = ET.fromstring(xml_style_str)
+
+    # for singleband_pseudocolor
+    items = xml_style.findall('pipe/rasterrenderer[@band="{}"]/rastershader/colorrampshader/item'.format(band))
+    if not items:
+        # for color table
+        items = xml_style.findall('pipe/rasterrenderer[@band="{}"]/colorPalette/paletteEntry'.format(band))
+
+    pixel_values = []
+    for item in items:
+        pixel_values.append(int(item.get("value")))
+    return pixel_values
+
+# --------------------------------------------------------------------------
+
+
+def chunks(l, n):
+    """generate the sub-list of chunks of n-sizes from list l"""
+    for i in range(0, len(l), n):
+        yield l[i:i + n]
+
+
+def pixel_count_in_chunk(args):
+    img_path, band, pixel_values, xoff, yoff, xsize, ysize = args
+    pixel_count = [0] * len(pixel_values)
+    gdal_file = gdal.Open(img_path, gdal.GA_ReadOnly)
+
+    chunk_narray = gdal_file.GetRasterBand(band).ReadAsArray(xoff, yoff, xsize, ysize).astype(np.int)
+
+    for idx, pixel_value in enumerate(pixel_values):
+        pixel_count[idx] += (chunk_narray == int(pixel_value)).sum()
+    return pixel_count
+
+
+@wait_process
+def get_pixel_count_by_pixel_values(layer, band, pixel_values=None):
+    """Get the total pixel count for each pixel values"""
+
+    if pixel_values is None:
+        pixel_values = get_pixel_values(layer, band)
+
+    # split the image in chunks, the 0,0 is left-upper corner
+    gdal_file = gdal.Open(get_file_path_of_layer(layer), gdal.GA_ReadOnly)
+    chunk_size = 1000
+    input_data = []
+    for y in chunks(range(gdal_file.RasterYSize), chunk_size):
+        yoff = y[0]
+        ysize = len(y)
+        for x in chunks(range(gdal_file.RasterXSize), chunk_size):
+            xoff = x[0]
+            xsize = len(x)
+
+            input_data.append((get_file_path_of_layer(layer), band, pixel_values, xoff, yoff, xsize, ysize))
+
+    # compute and merge all parallel process returns in one result
+    with multiprocessing.Pool(multiprocessing.cpu_count()) as pool:
+        imap_it = pool.imap(pixel_count_in_chunk, input_data)
+        pixel_counts = np.sum([proc for proc in imap_it], axis=0).tolist()
+        return dict(zip(pixel_values, pixel_counts))
+
+# --------------------------------------------------------------------------
