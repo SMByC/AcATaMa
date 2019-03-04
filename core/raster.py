f1f9d14b9eb0da10048a301a81e7653c9e6a0063
Xavier Corredor Llano <xcorredorl@ideam.gov.co>
2019-03-03 19:19:35 -0500

    Minor fix typos

--- core/raster.py
+++ core/raster.py
@@ -139,7 +139,7 @@ def get_singleband_pseudocolor(layer, band_number, nodata=None):
     # check if items is empty or any pixel value (in color table) not is integer
     if not items or False in [i.get("value").lstrip('+-').isdigit() for i in items]:
         msg = "The layer selected \"{}\" {}doesn't have an appropiate color style for AcATaMa, " \
-              "it must be singleband pseudocolor with integer values. " \
+              "it must be unique values or singleband pseudocolor with integer values. " \
               "<a href='https://smbyc.bitbucket.io/qgisplugins/acatama/how_to_use/#types-of-thematic-rasters-accepted-in-acatama'>" \
               "See more</a>.".format(layer.name(), "in the band {} ".format(band_number) if layer.bandCount() > 1 else "")
         QMessageBox.warning(None, 'Error reading the pixel color style...', msg)
