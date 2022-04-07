5b9ba747921228abc6306e0df3629aa50ecbcdee
XavierCLL <xavier.corredor.llano@gmail.com>
2022-04-06 23:23:35 -0700

    Deleted extra deps functions

--- __init__.py
+++ __init__.py
@@ -21,36 +21,6 @@
 """
 
 
-from qgis.PyQt.QtCore import QCoreApplication
-from qgis.PyQt.QtWidgets import QMessageBox
-
-from AcATaMa.utils.extra_deps import load_install_extra_deps, WaitDialog
-
-
-def pre_init_plugin(iface):
-    app = QCoreApplication.instance()
-    parent = iface.mainWindow()
-    dialog = None
-    log = ''
-    try:
-        for msg_type, msg_val in load_install_extra_deps():
-            app.processEvents()
-            if msg_type == 'log':
-                log += msg_val
-            elif msg_type == 'needs_install':
-                dialog = WaitDialog(parent, 'AcATaMa - installing dependencies')
-            elif msg_type == 'install_done':
-                dialog.accept()
-    except Exception as e:
-        if dialog:
-            dialog.accept()
-        QMessageBox.critical(parent, 'AcATaMa - installing dependencies',
-                             'An error occurred during the installation of Python packages. ' +
-                             'Click on "Stack Trace" in the QGIS message bar for details.')
-        raise RuntimeError('\nAcATaMa: Error installing Python packages. Read install instruction: '
-                           'https://smbyc.github.io/AcATaMa\nLog:\n' + log) from e
-
-
 # noinspection PyPep8Naming
 def classFactory(iface):  # pylint: disable=invalid-name
     """Load AcATaMa class from file AcATaMa.
@@ -58,8 +28,6 @@ def classFactory(iface):  # pylint: disable=invalid-name
     :param iface: A QGIS interface instance.
     :type iface: QgsInterface
     """
-    # load/install extra python dependencies
-    pre_init_plugin(iface)
 
     #
     from .acatama import AcATaMa
