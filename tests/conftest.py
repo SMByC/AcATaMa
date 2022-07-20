edcd61372693a6f6a813290569d834cf7f931490
XavierCLL <xavier.corredor.llano@gmail.com>
2022-07-20 12:21:06 -0700

    Cleaning conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,6 @@
 from pathlib import Path
 import pytest
 
-from AcATaMa import classFactory
-from AcATaMa.gui.acatama_dockwidget import AcATaMaDockWidget
 
 pytest_plugins = ('pytest_qgis',)
 
@@ -26,6 +24,8 @@ def plugin(pytestconfig, qgis_iface, qgis_parent, qgis_new_project):
     Initialize and return the plugin object.
     Resize the parent window according to config.
     """
+    from AcATaMa import classFactory
+    from AcATaMa.gui.acatama_dockwidget import AcATaMaDockWidget
 
     plugin = classFactory(qgis_iface)
     plugin.initGui()
@@ -34,12 +34,3 @@ def plugin(pytestconfig, qgis_iface, qgis_parent, qgis_new_project):
     yield plugin
     plugin.removes_temporary_files()
 
-
-@pytest.fixture
-def plugin_toolbar(pytestconfig, plugin, qgis_debug_enabled, qtbot):
-    toolbar = plugin.toolbar
-    toolbar.resize(int(pytestconfig.getini("qgis_window_width")), 70)
-    if qgis_debug_enabled:
-        toolbar.show()
-    qtbot.add_widget(toolbar)
-    yield toolbar
