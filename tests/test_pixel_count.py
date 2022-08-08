6099af805d248d1da3ae9d2fb47d575d06e5aa8f
XavierCLL <xavier.corredor.llano@gmail.com>
2022-08-08 01:45:13 -0700

    Added more tests for pixel count and auto symbology function

--- tests/test_pixel_count.py
+++ tests/test_pixel_count.py
@@ -1,17 +1,47 @@
+import os
 import pytest
+from osgeo import gdal
 
 from AcATaMa.core import config
+from AcATaMa.core.map import auto_symbology_classification_render
 from AcATaMa.utils.others_utils import get_pixel_count_by_pixel_values
+from AcATaMa.utils.qgis_utils import load_layer
 
 
-def test_pixel_count(plugin, unwrap):
+def test_pixel_count_without_nodata(plugin, unwrap):
     # restore
     input_yml_path = pytest.tests_data_dir / "test_pixel_count_acatama.yml"
     config_restore = unwrap(config.restore)
     config_restore(input_yml_path)
 
-    pixel_count = get_pixel_count_by_pixel_values(plugin.dockwidget.QCBox_ThematicMap.currentLayer(), band=2, nodata=None)
+    pixel_count = get_pixel_count_by_pixel_values(plugin.dockwidget.QCBox_ThematicMap.currentLayer(),
+                                                  band=2, nodata=None)
 
     assert pixel_count == {0: 3227, 34: 11, 35: 44, 36: 22, 37: 49, 38: 17, 40: 2, 42: 1, 43: 1,
                            44: 79, 45: 5, 46: 468, 47: 57, 49: 3, 51: 1, 52: 15, 53: 24}
 
+
+def test_pixel_count_with_nodata(plugin, unwrap):
+    # restore
+    input_yml_path = pytest.tests_data_dir / "test_pixel_count_acatama_nodata.yml"
+    config_restore = unwrap(config.restore)
+    config_restore(input_yml_path)
+
+    pixel_count = get_pixel_count_by_pixel_values(plugin.dockwidget.QCBox_ThematicMap.currentLayer(),
+                                                  band=int(plugin.dockwidget.QCBox_band_CategMap_StraRS.currentText()),
+                                                  nodata=float(plugin.dockwidget.nodata_CategMap_StraRS.text().strip() or "nan"))
+
+    assert pixel_count == {1: 10423, 2: 418, 5: 8822}
+
+
+def test_pixel_count_and_auto_symbology(plugin, unwrap, tmpdir):
+    # copy and unset the nodata
+    layer_unsetnodata = tmpdir.join('test_layer_with_nodata.tif')
+    gdal.Translate(str(layer_unsetnodata), str(pytest.tests_data_dir / "test_layer_with_nodata.tif"))
+    os.system("gdal_edit.py '{}' -unsetnodata".format(layer_unsetnodata))
+
+    layer = load_layer(str(layer_unsetnodata))
+    auto_symbology_classification_render(layer, 1)
+    pixel_count = get_pixel_count_by_pixel_values(layer, band=1, nodata="nan")
+
+    assert pixel_count == {-2147483647: 15785, 1: 10423, 2: 418, 5: 8822}
