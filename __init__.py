856eb3c90c50a09010a4c069211bbfe239d8946b
Xavier C. Llano <xavier.corredor.llano@gmail.com>
2023-03-02 23:35:11 -0800

    Include the external libs to enable by default computing the pixel count by pixel values in parrallel, it is more efficient in time and ram usage

--- __init__.py
+++ __init__.py
@@ -19,6 +19,25 @@
  ***************************************************************************/
  This script initializes the plugin, making it known to QGIS.
 """
+import os
+import platform
+import site
+import pkg_resources
+
+
+def pre_init_plugin():
+    if platform.system() == "Windows":
+        extlib_path = 'extlibs_windows'
+    if platform.system() == "Darwin":
+        extlib_path = 'extlibs_darwin'
+    if platform.system() == "Linux":
+        extlib_path = 'extlibs_linux'
+    extra_libs_path = os.path.abspath(os.path.join(os.path.dirname(__file__), extlib_path))
+
+    # add to python path
+    site.addsitedir(extra_libs_path)
+    # pkg_resources doesn't listen to changes on sys.path.
+    pkg_resources.working_set.add_entry(extra_libs_path)
 
 
 # noinspection PyPep8Naming
@@ -28,6 +47,8 @@ def classFactory(iface):  # pylint: disable=invalid-name
     :param iface: A QGIS interface instance.
     :type iface: QgsInterface
     """
+    # load extra python dependencies
+    pre_init_plugin()
 
     #
     from .acatama import AcATaMa
