e5ec451a508b2e0bcfd8c347d794733f45e8c35d
Xavier C. Llano <xavier.corredor.llano@gmail.com>
2023-03-05 13:08:22 -0800

    Added tests for counting pixel classes in parallel process

--- tests/test_pixel_count.py
+++ tests/test_pixel_count.py
@@ -4,30 +4,57 @@ from osgeo import gdal
 
 from AcATaMa.core import config
 from AcATaMa.core.map import auto_symbology_classification_render
-from AcATaMa.utils.others_utils import get_pixel_count_by_pixel_values
+from AcATaMa.utils.others_utils import get_pixel_count_by_pixel_values, get_pixel_count_by_pixel_values_sequential, \
+    get_pixel_count_by_pixel_values_parallel
 from AcATaMa.utils.qgis_utils import load_layer
 
 
-def test_pixel_count_without_nodata(plugin, unwrap):
+def test_pixel_count_without_nodata_sequential(plugin, unwrap):
     # restore
     input_yml_path = pytest.tests_data_dir / "test_pixel_count_acatama.yml"
     config_restore = unwrap(config.restore)
     config_restore(input_yml_path)
 
-    pixel_count = get_pixel_count_by_pixel_values(plugin.dockwidget.QCBox_ThematicMap.currentLayer(),
+    pixel_count = get_pixel_count_by_pixel_values_sequential(plugin.dockwidget.QCBox_ThematicMap.currentLayer(),
                                                   band=2, nodata=None)
 
     assert pixel_count == {0: 3227, 34: 11, 35: 44, 36: 22, 37: 49, 38: 17, 40: 2, 42: 1, 43: 1,
                            44: 79, 45: 5, 46: 468, 47: 57, 49: 3, 51: 1, 52: 15, 53: 24}
 
 
-def test_pixel_count_with_nodata(plugin, unwrap):
+def test_pixel_count_without_nodata_parallel(plugin, unwrap):
+    # restore
+    input_yml_path = pytest.tests_data_dir / "test_pixel_count_acatama.yml"
+    config_restore = unwrap(config.restore)
+    config_restore(input_yml_path)
+
+    pixel_count = get_pixel_count_by_pixel_values_parallel(plugin.dockwidget.QCBox_ThematicMap.currentLayer(),
+                                                  band=2, nodata=None)
+
+    assert pixel_count == {0: 3227, 34: 11, 35: 44, 36: 22, 37: 49, 38: 17, 40: 2, 42: 1, 43: 1,
+                           44: 79, 45: 5, 46: 468, 47: 57, 49: 3, 51: 1, 52: 15, 53: 24}
+
+
+def test_pixel_count_with_nodata_sequential(plugin, unwrap):
     # restore
     input_yml_path = pytest.tests_data_dir / "test_pixel_count_acatama_nodata.yml"
     config_restore = unwrap(config.restore)
     config_restore(input_yml_path)
 
-    pixel_count = get_pixel_count_by_pixel_values(plugin.dockwidget.QCBox_ThematicMap.currentLayer(),
+    pixel_count = get_pixel_count_by_pixel_values_sequential(plugin.dockwidget.QCBox_ThematicMap.currentLayer(),
+                                                  band=int(plugin.dockwidget.QCBox_band_CategMap_StraRS.currentText()),
+                                                  nodata=float(plugin.dockwidget.nodata_CategMap_StraRS.text().strip() or "nan"))
+
+    assert pixel_count == {1: 10423, 2: 418, 5: 8822}
+
+
+def test_pixel_count_with_nodata_parallel(plugin, unwrap):
+    # restore
+    input_yml_path = pytest.tests_data_dir / "test_pixel_count_acatama_nodata.yml"
+    config_restore = unwrap(config.restore)
+    config_restore(input_yml_path)
+
+    pixel_count = get_pixel_count_by_pixel_values_parallel(plugin.dockwidget.QCBox_ThematicMap.currentLayer(),
                                                   band=int(plugin.dockwidget.QCBox_band_CategMap_StraRS.currentText()),
                                                   nodata=float(plugin.dockwidget.nodata_CategMap_StraRS.text().strip() or "nan"))
 
