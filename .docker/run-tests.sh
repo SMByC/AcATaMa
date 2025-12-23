1960d00442a1fde604ffbc83d379533cb6921a20
Xavier C. Llano <xavier.corredor.llano@gmail.com>
2025-12-22 19:12:46 -0500

    Add GitHub release workflow and adjust some package files

--- .docker/run-tests.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Script to run tests locally using Docker
-# Usage: cd .docker && ./run-tests.sh [pytest args]
-
-set -e
-
-export GITHUB_WORKSPACE=$(dirname "$PWD")
-docker compose -f docker-compose.gh.yml run --rm qgis /usr/src/AcATaMa/.docker/run-docker-tests.sh "$@"
-docker compose -f docker-compose.gh.yml down
